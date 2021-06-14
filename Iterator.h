92be706dc5a4d55cca2a061b901ff6a182941f11
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-06-15 02:07:33 +0300

    Update Iterator.h

--- Iterator.h
+++ Iterator.h
@@ -8,7 +8,7 @@ class Iterator
     T* pos;
 
 public:
-    using iterator_category = std::random_access_iterator_tag;
+    using iterator_category = std::bidirectional_iterator_tag;
     using value_type = T;
     using difference_type = int;
     using pointer = T*;
@@ -18,10 +18,6 @@ public:
 
     bool operator==(const Iterator<T>& other) const { return pos == other.pos; };
     bool operator!=(const Iterator<T>& other) const { return pos != other.pos; };
-    bool operator>(const  Iterator<T>& other) const { return pos > other.pos; };
-    bool operator<(const  Iterator<T>& other) const { return pos < other.pos; };
-    bool operator>=(const Iterator<T>& other) const { return pos >= other.pos; };
-    bool operator<=(const Iterator<T>& other) const { return pos <= other.pos; };
 
     reference operator*() const { return *pos; };
     pointer operator->() const { return pos; };
@@ -50,28 +46,5 @@ public:
         Iterator<T> temp = *this;
         --pos;
         return temp;
-    };
-
-    reference operator[](size_t index) const { return pos[index]; };
-
-    Iterator operator+(const difference_type& diff) const { return Iterator<T>(pos + diff); };
-    friend inline Iterator operator+(const difference_type& diff, const Iterator<T>& other)
-    {
-        return Iterator<T>(other.pos + diff);
-    };
-
-    Iterator<T> operator-(const difference_type& diff) const { return Iterator<T>(pos - diff); };
-    difference_type operator-(const Iterator<T>& other) const { return std::distance(other.pos, pos); };
-
-    Iterator<T> operator+=(const difference_type& diff)
-    {
-        pos += diff;
-        return *this;
-    };
-
-    Iterator<T> operator-=(const difference_type& diff)
-    {
-        pos -= diff;
-        return *this;
     };
 };
