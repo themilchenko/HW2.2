8cbf980a804d4d1f0aa5be0d306f47f2dada9c25
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-06-15 02:09:25 +0300

    Update Set.h

--- Set.h
+++ Set.h
@@ -6,10 +6,11 @@
 #include "ReverseIterator.h"
 #include "ConstReverseIterator.h"
 
+#include "MyComparator.h"
+
 #pragma once
 
-
-template <class T> 
+template <class T, class Comp = std::less<T>>
 class Set
 {
 private:
@@ -17,20 +18,30 @@ private:
 	size_t _size;
 
 public:
-	Set() :  _size(0), _elements(nullptr) { };
-	Set(const Set<T>& other);
+	Set() :  _size(0), _elements(nullptr) 
+	{
+		_elements = new T[2];
+	};
+	Set(const Set<T, Comp>& other);
+
+	~Set()
+	{
+		if (_elements != nullptr)
+			delete[] _elements;
+		_size = 0;
+	}
 
 	Set& operator=(const Set<T>& other);
 
-	Iterator<T> begin()               noexcept          { return Iterator<T>(&_elements[0]);                 };
-	ConstIterator<T> cbegin()         const noexcept    { return ConstIterator<T>(&_elements[0]);            };
-	ReverseIterator<T> rbegin()       noexcept		    { return ReverseIterator<T>(&_elements[_size]);      };
-	ConstReverseIterator<T> crbegin() const noexcept    { return ConstReverseIterator<T>(&_elements[_size]); };
+	Iterator<T> begin()               noexcept          { return Iterator<T>(&_elements[1]);                        };
+	ConstIterator<T> cbegin()         const noexcept    { return ConstIterator<T>(&_elements[1]);                   };
+	ReverseIterator<T> rbegin()       noexcept		    { return ReverseIterator<T>(&_elements[_size - 1]); };
+	ConstReverseIterator<T> crbegin() const noexcept    { return ConstReverseIterator<T>(&_elements[_size - 1]);    };
 
-	Iterator<T> end()                 noexcept          { return Iterator<T>(&_elements[_size]);             };
-	ConstIterator<T> cend()			  const noexcept	{ return ConstIterator<T>(&_elements[_size]);        };
-	ReverseIterator<T> rend()         noexcept			{ return ReverseIterator<T>(&_elements[0]);          };
-	ConstReverseIterator<T> crend()   const noexcept	{ return ConstReverseIterator<T>(&_elements[0]);     };
+	Iterator<T> end()                 noexcept          { return Iterator<T>(&_elements[_size + 1]);                };
+	ConstIterator<T> cend()	          const noexcept    { return ConstIterator<T>(&_elements[_size + 1]);		};
+	ReverseIterator<T> rend()         noexcept	    { return ReverseIterator<T>(&_elements[0]);		        };
+	ConstReverseIterator<T> crend()   const noexcept    { return ConstReverseIterator<T>(&_elements[0]);		};
 
 	void insert(const T& elem);
 
@@ -50,4 +61,5 @@ public:
 
 	size_t count(const T& key) const;
 	Iterator<T> find(const T& key) const;
+	void merge(Set<T, Comp>& other);
 };
